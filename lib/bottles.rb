835b2b2f2a42e609263b3bf44521f472bc25de90
Caroline <mllegeorgesand@gmail.com>
2018-05-01 15:07:42 -0400

    Better syntax

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -5,40 +5,43 @@ class Bottles
     verses(99, 0)
   end
 
-  def verses(x, y)
-    array = []
-    (y..x).each do |index|
-      array << verse(index)
+  def verses(high, low)
+    high.downto(low).map do |index|
+      verse(index)
     end
-    array.reverse.join("\n")
+      .join("\n")
   end
 
-  def verse(x)
+  def verse(n)
     <<~VERSE
-      #{self.class.x_amount_of_bottle(x).capitalize} of beer on the wall, #{self.class.x_amount_of_bottle(x)} of beer.
-      #{self.class.instruction(x)}, #{self.class.x_amount_of_bottle(x - 1)} of beer on the wall.
+      #{self.class.x_amount_of_bottle(n).capitalize} of beer on the wall, #{self.class.x_amount_of_bottle(n)} of beer.
+      #{self.class.instruction(n)}, #{self.class.x_amount_of_bottle(n - 1)} of beer on the wall.
     VERSE
   end
 
-  def self.x_amount_of_bottle(amount)
-    if amount == -1
-      "99 bottles"
-    elsif amount == 0
-      "no more bottles"
-    elsif amount == 1
-      "1 bottle"
-    else
-      "#{amount} bottles"
-    end
-  end
+  private
 
-  def self.instruction(start)
-    if start == 0
-      'Go to the store and buy some more'
-    elsif start == 1
-      'Take it down and pass it around'
-    else
-      'Take one down and pass it around'
+  class << self
+    def x_amount_of_bottle(amount)
+      if amount == -1
+        "99 bottles"
+      elsif amount == 0
+        "no more bottles"
+      elsif amount == 1
+        "1 bottle"
+      else
+        "#{amount} bottles"
+      end
+    end
+
+    def instruction(number_of_bottles_we_started_with)
+      if number_of_bottles_we_started_with == 0
+        'Go to the store and buy some more'
+      elsif number_of_bottles_we_started_with == 1
+        'Take it down and pass it around'
+      else
+        'Take one down and pass it around'
+      end
     end
   end
 end
