1bd4b5bb2943799116022e1c224eb0a93b7efd31
Caroline <mllegeorgesand@gmail.com>
2018-05-27 17:05:09 -0400

    Simplest refactor

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -24,16 +24,19 @@ class Bottles
         1 bottle of beer on the wall, 1 bottle of beer.
         Take it down and pass it around, no more bottles of beer on the wall.
       VERSE
-    when 2
-      <<~VERSE
-        2 bottles of beer on the wall, 2 bottles of beer.
-        Take one down and pass it around, 1 bottle of beer on the wall.
-      VERSE
     else
       <<~VERSE
         #{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.
-        Take one down and pass it around, #{number_of_bottles - 1} bottles of beer on the wall.
+        Take one down and pass it around, #{number_of_bottles - 1} #{container(number_of_bottles - 1)} of beer on the wall.
       VERSE
+    end
+  end
+
+  def container(number)
+    if number == 1
+      'bottle'
+    else
+      'bottles'
     end
   end
 end
