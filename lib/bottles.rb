c0a67db9e3cd17cfdbfbb0a91daf8f20788f0cc2
Caroline <mllegeorgesand@gmail.com>
2018-05-27 22:48:42 -0400

    Finish refactoring to get 1 verse

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -12,23 +12,26 @@ class Bottles
       .join("\n")
   end
 
-  def verse(number_of_bottles)
-    case number_of_bottles
-    when 0
-      <<~VERSE
-        No more bottles of beer on the wall, no more bottles of beer.
-        Go to the store and buy some more, 99 bottles of beer on the wall.
-      VERSE
-    when 1
-      <<~VERSE
-        1 bottle of beer on the wall, 1 bottle of beer.
-        Take it down and pass it around, no more bottles of beer on the wall.
-      VERSE
+  def verse(number)
+    <<~VERSE
+      #{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.
+      #{action(number)}, #{quantity(successor(number))} #{container(successor(number))} of beer on the wall.
+    VERSE
+  end
+
+  def action(number)
+    if number == 0
+      'Go to the store and buy some more'
     else
-      <<~VERSE
-        #{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.
-        Take one down and pass it around, #{number_of_bottles - 1} #{container(number_of_bottles - 1)} of beer on the wall.
-      VERSE
+      "Take #{pronoun(number)} down and pass it around"
+    end
+  end
+
+  def quantity(number)
+    if number == 0
+      'no more'
+    else
+      number.to_s
     end
   end
 
@@ -37,6 +40,22 @@ class Bottles
       'bottle'
     else
       'bottles'
+    end
+  end
+
+  def pronoun(number)
+    if number == 1
+      'it'
+    else
+      'one'
+    end
+  end
+
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
     end
   end
 end
