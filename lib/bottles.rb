ab47971d61d059e6781111ea78da683a0d7ca201
Caroline <mllegeorgesand@gmail.com>
2018-05-28 14:28:03 -0400

    Just a little love

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -40,12 +40,12 @@ class BottleNumber
 end
 
 class BottleNumber0 < BottleNumber
-  def action
-    'Go to the store and buy some more'
-  end
-
   def quantity
     'no more'
+  end
+
+  def action
+    'Go to the store and buy some more'
   end
 
   def successor
